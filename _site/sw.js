5860c3a6fd1b6528847b2f98f4aa27957d4debe2
puar <45474252+puar-playground@users.noreply.github.com>
2023-05-16 02:40:00 -0400

    icon

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230516.023442';
+const cacheName = 'chirpy-20230516.023953';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
